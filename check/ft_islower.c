6db03e0c98bd7d8177866fc1b03448359c31f485
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-12-01 18:22:24 +0100

    Update ft_islower.c

--- check/ft_islower.c
+++ check/ft_islower.c
@@ -5,7 +5,7 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sperez-p <sperez-p@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/20 13:02:51 by sperez-p          #+#    #+#             */
+/*   Created: 2021/08/20 13:02:27 by sperez-p          #+#    #+#             */
 /*   Updated: 2021/12/01 16:35:22 by sperez-p         ###   ########.eus      */
 /*                                                                            */
 /* ************************************************************************** */
@@ -43,6 +43,7 @@
   When we return a condition, the computer will check if the condition is true or
   false. If it is true it will return 1 and if it is false it will
   return 0. This concept is known as Boolean values.
+  
 */
 
 int ft_islower(int c)
