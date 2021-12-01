a80712e525fd6b483a0924ea1ebc5d61eade6d82
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-12-01 15:55:18 +0100

    Create ft_isalnum.c

---
+++ ft_isalnum.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sperez-p <sperez-p@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/20 12:44:38 by sperez-p          #+#    #+#             */
+/*   Updated: 2021/12/01 15:52:34 by sperez-p         ###   ########.eus      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+  NAME
+  ****
+  ft_isalnum --> is an alphanumeric character
+
+  SYNOPSIS
+  ***********
+  #include "libft.h"
+  int	ft_isalnum(int c);
+
+  PARAMETERS
+  **********
+  c --> The character to be checked. It is passed as an int because characters
+        are interpeted as numbers from the ASCII table.
+
+  DESCRIPTION
+  ***********
+  The function ft_isalnum() mimics the behavior of the isalnum() function from
+  ctype C library. Both functions check if c is an alphanumeric character.
+
+  RETURN VALUE
+  ************
+  - Non-zero value if c is a digit or a letter.
+  - Zero value if not.
+*/
+
+int	ft_isalnum(int c)
+{
+	return (ft_isalpha(c) || ft_isdigit(c));
+}
