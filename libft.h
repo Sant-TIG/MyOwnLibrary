0470fb2ee967c3d839a66a78dc06a92d25161f46
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-12-01 15:22:52 +0100

    Create libft.h

---
+++ libft.h
@@ -0,0 +1,11 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <stdio.h>
+
+size_t	ft_intlen(int n);
+char	*ft_strcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_itoa(int n);
+
+#endif
