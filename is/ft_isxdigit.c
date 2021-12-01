db2947e8a2352fb08b52b4a42806974f619d912c
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-12-01 17:21:32 +0100

    Create ft_isxdigit.c

---
+++ is/ft_isxdigit.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isxdigit.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sperez-p <sperez-p@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/20 13:14:21 by sperez-p          #+#    #+#             */
+/*   Updated: 2021/12/01 17:18:20 by sperez-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+  NAME
+  ****
+  ft_isxdigit ---> is a hexadecimal digit character.
+
+  SYNOPSIS
+  ***********
+  #include "libft.h"
+  int	ft_isxdigit(int c);
+
+  PARAMETERS
+  **********
+  c --> The character to be checked. It is passed as an int because characters
+        are interpeted as numbers from the ASCII table.
+
+  DESCRIPTION
+  ***********
+  The function ft_isxdigit() mimics the behavior of the isxdigit() function from
+  ctype C library. Both functions check if c is a hexadecimal digit character
+  (0-9, a-f, A_F) or not.
+
+  RETURN VALUE
+  ************
+  - Non-zero value if c is a hexadecimal digit character.
+  - Zero value if not.
+  
+*/
+/*
+  RETURN A CONDITION
+  ******************
+  When we return a condition, the computer will check if the condition is true or
+  false. If it is true it will return 1 and if it is false it will
+  return 0. This concept is known as Boolean values.
+  
+*/
+
+int ft_isxdigit(int c)
+{
+    return(ft_isdigit(c) || ft_isxchar(c));
+}
